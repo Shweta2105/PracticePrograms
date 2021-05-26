1650354fa845708950f73b37150f769f941929be
Shweta2105 <sghadi21@gmail.com>
2021-05-26 17:50:54 +0530

    Added swap program

---
+++ src/SwapNumber.java
@@ -0,0 +1,22 @@
+
+public class SwapNumber {
+	public static void main(String[] args) {
+	//Swapping two numbers using 3rd variables
+		int a = 20;
+		int b = 30;
+		int temp;
+		System.out.println("Before swapping:" + a + " " + b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After swapping:" + a + " " + b);
+	//Swapping two numbers without using 3rd variable
+		
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("After swapping without using Temp variable:" + a + " " + b);
+	}
+	
+
+}
